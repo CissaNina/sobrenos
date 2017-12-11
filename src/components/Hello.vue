ec5586eda56d4d0ebc813b0ea9bd0ca22aeb970a
vainawebcpx <vainawebcpx@gmail.com>
2017-12-11 18:06:36 -0200

    addcomponent

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,24 +1,27 @@
-<template>
-  
+<template>  
   <section>
-    <h1>Sobre nós</h1>
-    <p>O ReciclAção propõe estimular e mobilizar a comunidade a
-        descartar adequadamente seus materiais recicláveis,
-        retirando-os do meio ambiente e dando origem a um ciclo fértil
-        e produtivo que retorna em forma de recursos locais, com
-        ênfase na educação e na cooperação</p>
-    <p>O Projeto reciclação começou 3 anos após o desabamento do
-        campinho, na Rua Gomes Lopes em 2010, provocado pelo
-        temporal e acúmulo de lixo nas encostas.36 pessoas morreram
-        e 7 mil ficaram desabrigadas. A partir disso o grupo PROA,
-        junto com a Cris dos Prazeres e sua equipe de trabalho, criaram
-        o projeto ReciclAção para promover ações de conservação e
-        conscientização ambiental no morro dos Prazeres,
-        concentradas no bem-estar das pessoas através do combate de
-        doenças como dengue, zica e chikungunya e outras doenças
-        causadas por acúmulo de lixo.</p>
-        <h3><a href="">Saiba mais >></a></h3>
-        <img src="../assets/01.jpg">
+    <div class="sobre-txt">
+      <h1>Sobre nós</h1>
+      <p>O ReciclAção propõe estimular e mobilizar a comunidade a
+          descartar adequadamente seus materiais recicláveis,
+          retirando-os do meio ambiente e dando origem a um ciclo fértil
+          e produtivo que retorna em forma de recursos locais, com
+          ênfase na educação e na cooperação</p>
+      <p>O Projeto reciclação começou 3 anos após o desabamento do
+          campinho, na Rua Gomes Lopes em 2010, provocado pelo
+          temporal e acúmulo de lixo nas encostas.36 pessoas morreram
+          e 7 mil ficaram desabrigadas. A partir disso o grupo PROA,
+          junto com a Cris dos Prazeres e sua equipe de trabalho, criaram
+          o projeto ReciclAção para promover ações de conservação e
+          conscientização ambiental no morro dos Prazeres,
+          concentradas no bem-estar das pessoas através do combate de
+          doenças como dengue, zica e chikungunya e outras doenças
+          causadas por acúmulo de lixo.</p>
+          <h3><a href="">Saiba mais >></a></h3>
+        </div>
+        <div class="sobre-img">
+            <img src="../assets/01.jpg">
+        </div>      
   </section>
 </template>
 
@@ -42,29 +45,35 @@ export default {
 </script>
 
 <style>
-      h1{ 
+body{
+  margin: 0 auto;
+  padding: 0 auto;
+}
+    div{
           display: flex;
           justify-content: flex-start;
-          align-items: flex-end;
+          flex-direction: column;
+          width: 100%;
+          margin: 0;
+          padding: 0;
+    } 
+      h1{ 
+          
           font-family: 'Roboto Slab, Bold';
           color: #00923F;
-          width: 195px;
-          height: 55px;
           size: 42px;
           text-align: left;
           padding-left: 21.32px;
           margin: 0;
-          font-size: 180%;
+          font-size: 1.5em;
         }
       p{
         font-family:'Roboto Slab, Regular';
         color: #000000;
-        width:330px;
-        size: 11px;
+        width:70%;
         opacity: 100%;
-        text-align: left;
         padding-left: 21.32px;
-        font-size: 80%;
+        font-size: 0.75em;
 
 
       }
@@ -87,6 +96,44 @@ export default {
 
     
 
-      img{width: 360px;
+      img{width: 305px;
           height:225.21px;  }
+
+
+            @media (min-width: 1024px) {
+
+              
+              section{
+                display: flex;
+              }
+
+              h1{
+                width: 413px;
+                height: 50px;
+                size: 89px;
+              }
+
+              p{
+                width: 65%;
+                size: 22px;
+                margin-left: 20px; 
+              }
+
+              h3{ 
+                  width: 282px;
+                  height: 23px;
+                  size:44px;
+                  font-size: 20px;
+                  }
+
+              img{
+                width: 30%;
+                height: 30%;
+                background:url(../assets/desabamento3.jpg) center center;;
+                background-repeat: no-repeat;
+                } 
+
+
+            }
+      
 </style>
--- src/components/resultados.vue
+++ src/components/resultados.vue
@@ -6,26 +6,26 @@
   
   	<ul>
   		<li>
-  			<img src="../assets/1.png">
-  			<p>18 toneladas de resíduos coletados em dois anos 7,460T de Garrafas Pet; 1,322T de alumínio; 3,688T de plástico duro; 3,851T de papelão; 1,789T de Longa Vida; 1320l de óleo usado</p>
+  			<img src="../assets/1.svg">
+  				<h3>18 toneladas de resíduos coletados em dois anos</h3> <p>7,460T de Garrafas Pet; 1,322T de alumínio; 3,688T de plástico duro; 3,851T de papelão; 1,789T de Longa Vida; 1320l de óleo usado</p>
 		</li>
-		<li><img src="../assets/2.png">
+		<li><img src="../assets/2.svg">
 			<p>Revitalização de espaços que costumavam servir como depósito de lixo irregular</p>
 		</li>
-		<li><img src="../assets/3.png">
+		<li><img src="../assets/3.svg">
 			<p>Melhor relação entre profissionais da Comlurb e moradores</p>
 		</li>
-		<li><img src="../assets/4.png">
+		<li><img src="../assets/4.svg">
 			<p>Valorização dos profissionais de limpeza Aumento do rendimento e produtividade	</p>
 		</li>
 		<li><img src="../assets/5.png">
 			<p>Comunidade mais limpa</p>
 		</li>
-		<li><img src="../assets/7.png">
+		<li><img src="../assets/6.svg">
 			<p>Caçambas de lixo cheias Maior conscientização ambiental</p>
 		</li>
 		<li>
-			<img src="../assets/8.png">
+			<img src="../assets/7.svg">
 			<p>Moradores com atitudes socio-ambientais</p>
 		</li>
 
@@ -57,9 +57,13 @@ export default {
 </script>
 
 <style>
-	
+	body{
+		box-sizing: border-box;
+		margin:  auto;
+		padding:  auto;
+	}
 	section{
-		width: 375px;
+		width: 100%;
 		height: 667px;
 		background: #7AC49F;
 	}
@@ -73,30 +77,56 @@ export default {
 		size: 42px;
 		color: #FFFFFF;
 		text-align: left;
-        padding-left: 21.32px;
+        padding-left: 25%;
         margin: 0;
         font-size: 225%;
+
 	}
+
+
 	ul {
+		display:flex;
+		flex-direction: row;
+		flex-wrap:wrap;
+		justify-content: center;
+		line-height: 0.7em;
+ 		width:100%;
+ 		height:84%;
      	list-style-type:none;
+     	padding-left:10px;
+
 
  }
 
- 	li{
- 		
+ 	li{		
+ 		width: 30%;
+ 		height: 20%;
+
 
  	}
 
 	img{
-		width: 82.9px;
-		height: 82.9px;
+		size: 3px;
+		width: 82.90px;
+		height: 82.90px;	
+  		margin: 0 .8%;
 	}
 
 	p{
 		font-family: 'Roboto Slab, Regular';
 		width: 95.7px;
 		height: 65.01px;
-		size: 8px;
+		font-size:8px;
+		color: #FFFFFF;
+		text-align: center;
+	}
 
+	h3{
+		font-family: 'Roboto Slab, Regular';
+		width: 95.7px;
+		height: 15.01px;
+		font-size:8px;
+		color: #FFFFFF;
+		text-align: center;
 	}
 </style>
